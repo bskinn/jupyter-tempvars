8009b37535210055b55ae53f1466393be28b84ed
Brian Skinn <brian.skinn@gmail.com>
2022-03-17 22:21:39 -0400

    Add first-stab extension functionality
    It works!! Definitely needs buffing/expansion, though.

---
+++ src/jupyter_tempvars/extension/jupyter_tempvars.js
@@ -0,0 +1,50 @@
+/**
+ * jupyter_tempvars.js
+ * 
+ * (needs improving)
+ * Wrap all cell-executed code with a TempVars context manager
+ * to handle temporary variables.
+ * 
+ * 
+ * @author  Brian Skinn, https://github.com/bskinn
+ * 
+ */
+
+define([
+  'notebook/js/codecell'
+], function (codecell) {
+
+    "use strict";
+
+    var CodeCell = codecell.CodeCell
+
+    return {
+        load_ipython_extension: function () {
+            console.log('[jupyter-tempvars] patching CodeCell.execute');
+            var orig_execute = CodeCell.prototype.execute;
+
+            CodeCell.prototype.execute = function (stop_on_error) {
+                const is_tempvars = (e) => e == "tempvars";
+
+                var tags = this.metadata.tags || [];  // Could do this by inspecting first line of cell text, too
+
+                if (tags.some(is_tempvars)) {
+                    var orig_text = this.get_text();
+                    
+                    var new_text = orig_text;
+
+                    new_text = new_text.replace("\n", "\n    ");
+                    new_text = "from tempvars import TempVars\nwith TempVars(starts=['t_']):\n    " + new_text;
+
+                    this.set_text(new_text);
+
+                    orig_execute.call(this, stop_on_error);
+
+                    this.set_text(orig_text);
+                } else {
+                    orig_execute.call(this, stop_on_error);
+                }
+            };
+        }
+    };
+});
