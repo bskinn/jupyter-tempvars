19623d739117af19fa66a1a6c88b7e5fe98c25a6
Brian Skinn <brian.skinn@gmail.com>
2022-03-23 13:46:13 -0400

    Clean up code and add console log helper

--- src/jupyter_tempvars/extension/jupyter_tempvars.js
+++ src/jupyter_tempvars/extension/jupyter_tempvars.js
@@ -26,37 +26,42 @@ define([
     var comm_name = "_jupyter_tempvars_comm"
     var settings
 
+    var c_log = (msg) => {console.log(`[jupyter-tempvars] ${msg}`);};
+
     return {
         load_ipython_extension: function () {
 
             var register_comms = () => {
+                c_log("registering kernel comm");
                 Jupyter.notebook.kernel.execute(
                     `if "_Comm" not in dir():
                         from ipykernel.comm import Comm as _Comm
-                        _jupyter_tempvars_comm = _Comm(target_name='${comm_name}')`
+                        _jupyter_tempvars_comm = _Comm(target_name='${comm_name}')
+                    `
                 );
 
-                if ( ! (comm_name in Jupyter.notebook.kernel.comm_manager.targets)) {
-                    Jupyter.notebook.kernel.comm_manager.register_target(
-                        comm_name,
-                        (comm, msg) => {
-                            comm.on_msg( m => {settings = m.content.data})
-                        }
-                    );
-                }
-
+                c_log("registering frontend comm");
+                Jupyter.notebook.kernel.comm_manager.register_target(
+                    comm_name,
+                    (comm, msg) => {
+                        comm.on_msg( m => {settings = m.content.data})
+                    }
+                );
             }
 
-            console.log('[jupyter-tempvars] patching CodeCell.execute');
+            c_log('patching CodeCell.execute');
             var orig_execute = CodeCell.prototype.execute;
 
             CodeCell.prototype.execute = function (stop_on_error) {
                 /* Register comms if needed */
-                register_comms()
+                if ( ! (comm_name in Jupyter.notebook.kernel.comm_manager.targets)) {
+                    register_comms();
+                }
 
-                console.log('[jupyter-tempvars] retrieving settings')
+                c_log('retrieving settings');
                 Jupyter.notebook.kernel.execute("_jupyter_tempvars_comm.send(_jupyter_tempvars_settings)")
-                console.log(settings)
+                setTimeout(() => {console.log(settings)}, 2000)
+                // console.log(settings)
 
                 const is_tempvars = (e) => e == "tempvars";
 
