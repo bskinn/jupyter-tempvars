ffaa67178605d92c63234426a3e1fa3399af9058
Brian Skinn <brian.skinn@gmail.com>
2022-03-23 17:11:22 -0400

    Complete initial POC
    IPython magic is now fully obsolete, with this implementation
    that just goes off of each cell's tags.

--- src/jupyter_tempvars/extension/jupyter_tempvars.js
+++ src/jupyter_tempvars/extension/jupyter_tempvars.js
@@ -23,62 +23,40 @@ define([
     "use strict";
 
     var CodeCell = codecell.CodeCell
-    var comm_name = "_jupyter_tempvars_comm"
-    var settings
-
     var c_log = (msg) => {console.log(`[jupyter-tempvars] ${msg}`);};
 
     return {
         load_ipython_extension: function () {
-
-            var register_comms = () => {
-                c_log("registering kernel comm");
-                Jupyter.notebook.kernel.execute(
-                    `if "_Comm" not in dir():
-                        from ipykernel.comm import Comm as _Comm
-                        _jupyter_tempvars_comm = _Comm(target_name='${comm_name}')
-                    `
-                );
-
-                c_log("registering frontend comm");
-                Jupyter.notebook.kernel.comm_manager.register_target(
-                    comm_name,
-                    (comm, msg) => {
-                        comm.on_msg( m => {settings = m.content.data})
-                    }
-                );
-            }
+            c_log('importing TempVars');
+            /* Timeout is needed because kernel isn't immediately available when extension
+             * loading is initiated */
+            setTimeout(() => {Jupyter.notebook.kernel.execute("from tempvars import TempVars");}, 2000);
 
             c_log('patching CodeCell.execute');
             var orig_execute = CodeCell.prototype.execute;
 
             CodeCell.prototype.execute = function (stop_on_error) {
-                /* Register comms if needed */
-                if ( ! (comm_name in Jupyter.notebook.kernel.comm_manager.targets)) {
-                    register_comms();
-                }
-
-                c_log('retrieving settings');
-                Jupyter.notebook.kernel.execute("_jupyter_tempvars_comm.send(_jupyter_tempvars_settings)")
-                setTimeout(() => {console.log(settings)}, 2000)
-                // console.log(settings)
-
-                const is_tempvars = (e) => e == "tempvars";
+                const is_tempvars = (tag) => tag.startsWith("tempvars");
 
                 var tags = this.metadata.tags || [];  // Could do this by inspecting first line of cell text, too
 
                 if (tags.some(is_tempvars)) {
                     var orig_text = this.get_text();
-
                     var new_text = orig_text;
 
-                    new_text = new_text.replace("\n", "\n    ");
-                    new_text = "from tempvars import TempVars\nwith TempVars(starts=['t_']):\n    " + new_text;
+                    tags.forEach( tag => {  // Switch to accumulating all starts/ends, and only using one TempVars?
+                        if ( tag.startsWith("tempvars-start-") ) {
+                            new_text = new_text.replaceAll("\n", "\n    ");
+                            new_text = `with TempVars(starts=['${tag.substring(15)}']):\n    ${new_text}`;
+                        }
+                        if ( tag.startsWith("tempvars-end-") ) {
+                            new_text = new_text.replaceAll("\n", "\n    ");
+                            new_text = `with TempVars(ends=['${tag.substring(13)}']):\n    ${new_text}`;
+                        }
+                    });
 
                     this.set_text(new_text);
-
                     orig_execute.call(this, stop_on_error);
-
                     this.set_text(orig_text);
                 } else {
                     orig_execute.call(this, stop_on_error);
