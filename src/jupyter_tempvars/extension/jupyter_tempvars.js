b7267f0dc090629af87f752298bd183078b3563b
Brian Skinn <brian.skinn@gmail.com>
2022-03-23 12:01:45 -0400

    Make first pass at settings retrieval

--- src/jupyter_tempvars/extension/jupyter_tempvars.js
+++ src/jupyter_tempvars/extension/jupyter_tempvars.js
@@ -1,18 +1,18 @@
 /**
  * jupyter_tempvars.js
- * 
+ *
  * (needs improving)
  * Wrap all cell-executed code with a TempVars context manager
  * to handle temporary variables.
- * 
+ *
  * Code-patching implementation adapted from the execution_dependencies
  * Jupyter extension:
- * 
+ *
  *  https://github.com/ipython-contrib/jupyter_contrib_nbextensions/tree/master/src/jupyter_contrib_nbextensions/nbextensions/execution_dependencies
- * 
- * 
+ *
+ *
  * @author  Brian Skinn, https://github.com/bskinn
- * 
+ *
  */
 
 define([
@@ -23,32 +23,50 @@ define([
     "use strict";
 
     var CodeCell = codecell.CodeCell
+    var comm_name = "_jupyter_tempvars_comm"
+    // var settings
 
     return {
         load_ipython_extension: function () {
-            console.log('[jupyter-tempvars] initializing metadata')
-            let md = Jupyter.notebook.metadata;
-            md["tempvars"] = {};
-            let tv = md["tempvars"];
 
-            tv["universal"] = false;
-            tv["start_with"] = [];
-            tv["end_with"] = [];
+            var register_comms = () => {
+                console.log('[jupyter-tempvars] registering communicator endpoints')
 
-            console.log('[jupyter-tempvars] defining helpers');
-            
+                if ( ! comm_name in Jupyter.notebook.kernel.comm_manager.comms) {
+                    Jupyter.notebook.kernel.comm_manager.register_target(
+                        comm_name,
+                        (comm, msg) => {
+                            comm.on_msg( m => {console._jupyter_tempvars_retval = m.content.data})
+                        }
+                    );
+                }
+
+                Jupyter.notebook.kernel.execute(
+                    `if "_Comm" not in dir():
+                        from ipykernel.comm import Comm as _Comm
+                        _jupyter_tempvars_comm = _Comm(target_name='${comm_name}')`
+                );
+            }
 
             console.log('[jupyter-tempvars] patching CodeCell.execute');
             var orig_execute = CodeCell.prototype.execute;
 
             CodeCell.prototype.execute = function (stop_on_error) {
+                /* Register comms if needed */
+                register_comms()
+
+                console.log('[jupyter-tempvars] retrieving settings')
+                Jupyter.notebook.kernel.execute("_jupyter_tempvars_comm.send(_jupyter_tempvars_settings)")
+                var settings = console._jupyter_tempvars_retval
+                console.log(settings)
+
                 const is_tempvars = (e) => e == "tempvars";
 
                 var tags = this.metadata.tags || [];  // Could do this by inspecting first line of cell text, too
 
                 if (tags.some(is_tempvars)) {
                     var orig_text = this.get_text();
-                    
+
                     var new_text = orig_text;
 
                     new_text = new_text.replace("\n", "\n    ");
